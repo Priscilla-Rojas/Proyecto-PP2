a2e1d088574239c710f5db671154e7dcb4e06746
Priscilla Rojas <priscilla.k.rojas@hotmail.com>
2026-06-28 17:19:06 -0300

    chore: removimos un  archivo en desuso

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import './styles/main.css'
-
-// Clase 2:
-// - agregar selectores del DOM
-// - escuchar eventos
-// - validar formulario
-// - mostrar u ocultar contenido
-// - conectar comportamiento a la sección de contacto o a la navegación
